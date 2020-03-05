e926522e305968aecf6e4cf5239e71a810b186f1
Niklas Fahl <niklas.fahl@ua.edu>
2020-03-05 15:21:25 -0600

    - fixed inconsistencies in SettingsCoordinator

--- CoordinatorAndRepositoryPatterns/Coordinators/SettingsCoordinator.swift
+++ CoordinatorAndRepositoryPatterns/Coordinators/SettingsCoordinator.swift
@@ -34,21 +34,25 @@ extension SettingsCoordinator: SettingsViewControllerDelegate {
         settingsDetailsVC.delegate = self
         settingsDetailsVC.showsDoneButton = navigationController?.presentingViewController != nil
         
+        print("showing settings details...")
         navigationController?.pushViewController(settingsDetailsVC, animated: true)
     }
     
     func leaveSettings() {
         // This is called both when user navigates back out of settings and when user taps the done button (if any) to leave the settings page in a modal context
+        let isNavigationControllerPresentedModally = navigationController?.presentingViewController != nil
+        let isLeavingControllerLastOnNavigationStack = navigationController?.viewControllers.last is SettingsViewController
+        let isLeavingControllerSecondToLastOnNavigationStack = navigationController?.viewControllers.dropLast().last is SettingsViewController
         
-        if navigationController?.viewControllers.dropLast().last == nil {
-            // Settings controller is last on nav stack. Remove from parent coordinator and dismiss if modally presented
+        if isNavigationControllerPresentedModally && isLeavingControllerLastOnNavigationStack {
+            // Settings controller is last on nav stack. Remove from parent coordinator and dismiss if modally presented.
             parentCoordinator?.childDidFinish(self)
             print("if needed, save or update data here")
-            
-            // Check if navigation controller is presented modally
-            if navigationController?.presentingViewController != nil {
-                navigationController?.dismiss(animated: true)
-            }
+            navigationController?.dismiss(animated: true)
+        } else if !isLeavingControllerSecondToLastOnNavigationStack && !isLeavingControllerLastOnNavigationStack {
+            // Settings was popped from nav stack, remove from parent coordinator
+            parentCoordinator?.childDidFinish(self)
+            print("if needed, save or update data here")
         }
     }
 }
@@ -56,14 +60,16 @@ extension SettingsCoordinator: SettingsViewControllerDelegate {
 extension SettingsCoordinator: SettingsDetailsViewControllerDelegate {
     func leaveSettingsDetails() {
         // Is modal and settings details is last on nav stack?
-        if navigationController?.presentingViewController != nil && navigationController?.viewControllers.last is SettingsDetailsViewController {
-            // Settings view controller is last on nav stack, so dismiss the settings coordinator stack
+        let isNavigationControllerPresentedModally = navigationController?.presentingViewController != nil
+        let isLeavingControllerLastOnNavigationStack = navigationController?.viewControllers.last is SettingsDetailsViewController
+        let isLeavingControllerSecondToLastOnNavigationStack = navigationController?.viewControllers.dropLast().last is SettingsDetailsViewController
+        
+        if isNavigationControllerPresentedModally && isLeavingControllerLastOnNavigationStack {
+            // Settings view controller is last on nav stack. Remove from parent coordinator and dismiss if modally presented.
             parentCoordinator?.childDidFinish(self)
             print("if needed, save or update data here")
             navigationController?.dismiss(animated: true)
-            
-            // Note: If this was leading to another controller you'd push that one here instead of dismissing and sending message to parent coordinator
-        } else {
+        } else if !isLeavingControllerSecondToLastOnNavigationStack && !isLeavingControllerLastOnNavigationStack {
             print("do something when settings details are popped from nav stack")
         }
     }
